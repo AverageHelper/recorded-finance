516bcc2a5bfc53267ec91150726937844fbbca29
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-08-16 17:05:11 -0600

    Move data export code into authStore

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,13 +1,18 @@
-import type { DatabaseSchema } from "../model/DatabaseSchema";
+import type { AttachmentSchema, DatabaseSchema } from "../model/DatabaseSchema";
+import type { Attachment } from "../model/Attachment";
 import type { HashStore, KeyMaterial, Unsubscribe, UserPreferences } from "../transport";
 import type { User } from "../transport/auth.js";
 import { AccountableError } from "../transport/errors";
+import { attachment as newAttachment } from "../model/Attachment";
+import { BlobReader, Data64URIWriter, TextReader, ZipWriter } from "@zip.js/zip.js";
 import { bootstrap, updateUserStats } from "./uiStore";
 import { get, writable } from "svelte/store";
 import { t } from "../i18n";
 import { UnauthorizedError } from "../../server/errors";
 import { v4 as uuid } from "uuid";
 import {
+	asyncMap,
+	dataUriToBlob,
 	defaultPrefs,
 	deleteAuthMaterial,
 	deleteUserPreferences,
@@ -329,4 +334,80 @@ export async function getAllUserDataAsJson(): Promise<DatabaseSchema> {
 		locations,
 		tags,
 	};
+}
+
+/** Compresses the user's data into a data URI string. */
+export async function compressUserData(shouldMinify: boolean): Promise<string> {
+	// TODO: Investigate replacing zip.js with https://www.npmjs.com/package/jszip
+
+	console.debug("Preparing zip writer");
+	const writer = new ZipWriter(new Data64URIWriter("application/zip"));
+	console.debug("Prepared zip writer");
+
+	try {
+		const rootName = "accountable";
+		console.debug("Writing root folder");
+		console.debug("Wrote root folder");
+
+		// ** Prepare database
+		console.debug("Getting user data");
+		const rawData = await getAllUserDataAsJson();
+		console.debug("Got user data");
+		console.debug("Encoding user data");
+		const data = JSON.stringify(rawData, undefined, shouldMinify ? undefined : "\t");
+		const encodedData = data;
+		console.debug("Encoded user data");
+		console.debug("Writing user data");
+		await writer.add(
+			`${rootName}/database${shouldMinify ? "-raw" : ""}.json`,
+			new TextReader(encodedData)
+		);
+		console.debug("Wrote user data");
+
+		// ** Prepare attachments
+
+		// dynamic import because attachmentsStore imports authStore lol
+		const { imageDataFromFile } = await import("./attachmentsStore");
+
+		/** Mirrors the storage bucket layout */
+		const userFilesPath = `${rootName}/storage/users/${rawData.uid}/attachments`;
+
+		const filesToGet: Array<AttachmentSchema> = rawData.attachments ?? [];
+		// FIXME: We may run out of memory here. Test with many files totaling more than 1 GB. Maybe operate on the attachments a few at a time?
+		console.debug("Downloading attachments");
+		const filesGotten: Array<[Attachment, string]> = await asyncMap(filesToGet, async a => {
+			const file = newAttachment({
+				createdAt: a.createdAt,
+				id: a.id,
+				notes: a.notes?.trim() ?? null,
+				storagePath: a.storagePath,
+				title: a.title.trim(),
+				type: a.type ?? "",
+			});
+			const data = await imageDataFromFile(file, false);
+			return [file, data];
+		});
+		console.debug("Downloaded attachments");
+		for (const [f, d] of filesGotten) {
+			// Get storage file name without extension, so that explorers don't try to treat the folder as JSON
+			const mystifiedName = f.storagePath
+				.slice(Math.max(0, f.storagePath.lastIndexOf("/") + 1))
+				.split(".")[0] as string;
+			const imagePath = `${userFilesPath}/${mystifiedName}/${f.title}`;
+			const image = dataUriToBlob(d);
+			console.debug(`Adding attachment ${f.title} to zip`);
+			await writer.add(imagePath, new BlobReader(image));
+			console.debug(`Added attachment ${f.title} to zip`);
+		}
+
+		// ** Zip them up
+		console.debug("Grabbing zip blob");
+		const dataUri = await writer.close();
+		console.debug(`Got ${dataUri.length}-byte zip blob`);
+
+		return dataUri;
+	} catch (error) {
+		await writer.close();
+		throw error;
+	}
 }
