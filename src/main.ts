93bc57f93c6115aa508b69e793f8593736b45bd9
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-08-21 15:48:18 -0600

    Better JavaScript fallback message

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import App from "./App.svelte";
 
+// Remove the fallback element. This won't happen if JS is disabled.
+document.querySelector("#noscript")?.remove();
+
 // Initialize Svelte app
 new App({
 	target: document.body,
