c3c756937029983b5218ec4378d0d6e11671b740
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-09-08 14:15:09 -0600

    feat(release): Print the diff of the changelog when we fix it

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,6 +1,7 @@
 import { parser } from "keep-a-changelog";
 import { readFileSync, writeFileSync } from "node:fs";
 import { URL } from "node:url";
+import gitDiff from "git-diff";
 
 // fix the changelog's footer links and bumps the `version` in [package.json](/package.json) and [package-lock.json](/package-lock.json).
 
@@ -9,19 +10,22 @@ function quote(str: string | undefined): string | undefined {
 	return `'${str}'`;
 }
 
+console.info("** release.ts **");
+
 // Load the changelog
 const changelogPath = new URL("../CHANGELOG.md", import.meta.url).pathname;
-console.info(`Loading changelog from '${changelogPath}'\n`);
+console.info("Loading changelog from", quote(changelogPath));
 
-const changelog = parser(readFileSync(changelogPath, "utf-8"));
-console.info(`${changelog.description}\n`);
+const rawChangelog = readFileSync(changelogPath, "utf-8");
+const changelog = parser(rawChangelog);
 
 const releases = changelog.releases;
 
 // Get 'Unreleased' if it exists
 const unreleasedRelease = releases[0];
-const unreleasedReleaseCompareLink = unreleasedRelease?.getCompareLink(changelog);
-console.info("unreleasedReleaseCompareLink:", quote(unreleasedReleaseCompareLink));
+if (unreleasedRelease) {
+	console.info("Has 'Unreleased'");
+}
 
 // Get current versioned release
 const thisReleaseIdx = releases.findIndex(release => release.date && release.version);
@@ -31,14 +35,23 @@ if (!thisRelease?.version) throw new TypeError("No versioned release was found."
 // Handy info
 console.info("latest release:", thisRelease.version?.toString());
 
-const compareLink = thisRelease.getCompareLink(changelog);
-console.info("compare link:", quote(compareLink));
-
 const prevRelease = releases[thisReleaseIdx + 1];
 console.info("previous release:", prevRelease?.version?.toString());
 
-// Fix the changelog's format (new compare links, etc.)
+// Fix the changelog's format (new compare links, etc.), and print the diff of our changes
+console.info("\n** Spec compliance **");
+
 const newChangelog = changelog.toString();
-console.info(`Writing new changelog to '${changelogPath}'`);
 writeFileSync(changelogPath, newChangelog);
-console.info("Written!");
+
+const diff = gitDiff(rawChangelog, newChangelog, { color: true });
+const didFix = diff !== undefined;
+if (!didFix) {
+	console.info("Changelog was already spec compliant.");
+} else {
+	console.info("Fixed formatting for spec compliance:");
+	console.info(diff);
+}
+
+if (didFix)
+	throw new EvalError("CHANGELOG.md was not spec compliant. Please review changes and re-run."); // this should fail us in CI
