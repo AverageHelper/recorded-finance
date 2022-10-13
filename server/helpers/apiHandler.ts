a2daf19c179d941076f34c499cdca3baf3fe95f6
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-10-13 13:32:40 -0600

    fix: Alow common headers

--- server/helpers/apiHandler.ts
+++ server/helpers/apiHandler.ts
@@ -60,4 +60,8 @@ function assertCors(req: APIRequest, res: APIResponse): void {
 	console.debug(`Handling request from origin: ${origin}`);
 	res.setHeader("Access-Control-Allow-Origin", origin);
 	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+	);
 }
