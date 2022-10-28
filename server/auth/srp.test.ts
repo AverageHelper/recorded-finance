23d7e8fc942d1fc42f7640b24ff9dd5a2f73f5e9
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-10-27 18:20:54 -0600

    feat: Working SRP core implementation

---
+++ server/auth/srp.test.ts
@@ -0,0 +1,322 @@
+import vectors from "./srp-test-vectors.json";
+import {
+	assertHashAlgorithm,
+	clientPremasterSecret,
+	clientPublicValue,
+	computeK,
+	computeU,
+	computeX,
+	HASH,
+	modExp,
+	PAD,
+	randomBits,
+	serverPremasterSecret,
+	serverPublicValue,
+	verifier,
+} from "./srp";
+
+describe("SRP", () => {
+	function bigintFromHex(hex: string): bigint {
+		return BigInt(
+			`0x${hex}` //
+				.replace(/ /gu, "")
+				.replace(/\n/gu, "")
+				.replace(/\t/gu, "")
+		);
+	}
+
+	type Numberified<T extends object, E extends Array<keyof T>> = {
+		[K in keyof T]: T[K] extends string ? (Array<K> extends E ? T[K] : bigint) : T[K];
+	};
+
+	function keysToBigInt<T extends object, E extends Array<keyof T>>(
+		obj: T,
+		exceptKeys: E
+	): Numberified<T, E> {
+		const result = { ...obj };
+		for (const key of Object.keys(result)) {
+			// Ignore exempted keys
+			if (exceptKeys.includes(key as keyof T)) continue;
+
+			// Transform string values into bigint
+			const og = result[key as keyof T];
+			if (typeof og === "string") {
+				result[key as keyof T] = bigintFromHex(og) as T[keyof T];
+			}
+		}
+		return result as Numberified<T, E>;
+	}
+
+	describe("matches Appendix B vectors", () => {
+		const I = "alice";
+		const P = "password123";
+		const H = "sha1";
+		const s = bigintFromHex(`BEB25379 D1A8581E B5A72767 3A2441EE`);
+		const N = bigintFromHex(`
+EEAF0AB9 ADB38DD6 9C33F80A FA8FC5E8 60726187 75FF3C0B 9EA2314C
+9C256576 D674DF74 96EA81D3 383B4813 D692C6E0 E0D5D8E2 50B98BE4
+8E495C1D 6089DAD1 5DC7D7B4 6154D6B6 CE8EF4AD 69B15D49 82559B29
+7BCF1885 C529F566 660E57EC 68EDBC3C 05726CC0 2FD4CBF4 976EAA9A
+FD5138FE 8376435B 9FC61D2F C0EB06E3`);
+		const g = 2n;
+		const k = bigintFromHex(`7556AA04 5AEF2CDD 07ABAF0F 665C3E81 8913186F`);
+		const x = bigintFromHex(`94B7555A ABE9127C C58CCF49 93DB6CF8 4D16C124`);
+		const v = bigintFromHex(`
+7E273DE8 696FFC4F 4E337D05 B4B375BE B0DDE156 9E8FA00A 9886D812
+9BADA1F1 822223CA 1A605B53 0E379BA4 729FDC59 F105B478 7E5186F5
+C671085A 1447B52A 48CF1970 B4FB6F84 00BBF4CE BFBB1681 52E08AB5
+EA53D15C 1AFF87B2 B9DA6E04 E058AD51 CC72BFC9 033B564E 26480D78
+E955A5E2 9E7AB245 DB2BE315 E2099AFB`);
+		const a = bigintFromHex(
+			`60975527 035CF2AD 1989806F 0407210B C81EDC04 E2762A56 AFD529DD DA2D4393`
+		);
+		const b = bigintFromHex(
+			`E487CB59 D31AC550 471E81F0 0F6928E0 1DDA08E9 74A004F4 9E61F5D1 05284D20`
+		);
+		const A = bigintFromHex(`
+61D5E490 F6F1B795 47B0704C 436F523D D0E560F0 C64115BB 72557EC4
+4352E890 3211C046 92272D8B 2D1A5358 A2CF1B6E 0BFCF99F 921530EC
+8E393561 79EAE45E 42BA92AE ACED8251 71E1E8B9 AF6D9C03 E1327F44
+BE087EF0 6530E69F 66615261 EEF54073 CA11CF58 58F0EDFD FE15EFEA
+B349EF5D 76988A36 72FAC47B 0769447B`);
+		const B = bigintFromHex(`
+BD0C6151 2C692C0C B6D041FA 01BB152D 4916A1E7 7AF46AE1 05393011
+BAF38964 DC46A067 0DD125B9 5A981652 236F99D9 B681CBF8 7837EC99
+6C6DA044 53728610 D0C6DDB5 8B318885 D7D82C7F 8DEB75CE 7BD4FBAA
+37089E6F 9C6059F3 88838E7A 00030B33 1EB76840 910440B1 B27AAEAE
+EB4012B7 D7665238 A8E3FB00 4B117B58`);
+		const u = bigintFromHex(`CE38B959 3487DA98 554ED47D 70A7AE5F 462EF019`);
+		const premasterSecret = bigintFromHex(`
+B0DC82BA BCF30674 AE450C02 87745E79 90A3381F 63B387AA F271A10D
+233861E3 59B48220 F7C4693C 9AE12B0A 6F67809F 0876E2D0 13800D6C
+41BB59B6 D5979B5C 00A172B4 A2A5903A 0BDCAF8A 709585EB 2AFAFA8F
+3499B200 210DCC1F 10EB3394 3CD67FC8 8A2F39A4 BE5BEC4E C0A3212D
+C346D7E4 74B29EDE 8A469FFE CA686E5A`);
+
+		test("matches u", () => {
+			expect(computeU(A, B, N, H)).toBe(u);
+		});
+
+		test("matches k", () => {
+			expect(computeK(N, g, H)).toBe(k);
+		});
+
+		test("matches x", () => {
+			expect(computeX(s, I, P, H)).toBe(x);
+		});
+
+		test("matches v", () => {
+			expect(verifier(s, I, P, N, g, H)).toBe(v);
+		});
+
+		test("matches A", () => {
+			expect(clientPublicValue(a, N, g)).toBe(A);
+		});
+
+		test("matches B", () => {
+			expect(serverPublicValue(b, v, N, g, H)).toBe(B);
+		});
+
+		test("matches premaster secret on client", () => {
+			expect(clientPremasterSecret(a, I, P, { N, g, s, B }, H)).toBe(premasterSecret);
+		});
+
+		test("matches premaster secret on server", () => {
+			expect(serverPremasterSecret(b, N, g, v, { A }, H)).toBe(premasterSecret);
+		});
+	});
+
+	describe.each(vectors.testVectors)(
+		"matches srptools test vectors (hash: $H, prime: $size bits)",
+		params => {
+			const { H, size, N, g, I, P, s, k, x, v, a, b, A, B, u, S, K, M1, M2 } = keysToBigInt(
+				params,
+				["H", "I", "P"]
+			);
+
+			// Make sure we only test an algorithm we know
+			assertHashAlgorithm(H);
+
+			// Sanity check that N is the given size
+			const Nstr = N.toString(16);
+			const byteCount = (Nstr.length / 2) * 8;
+			if (byteCount !== size)
+				throw new TypeError(`Expected N to be ${size} bytes, but actually is ${byteCount} bytes`);
+
+			// Check that our implementation returns the correct values
+			test("matches u", () => {
+				expect(computeU(A, B, N, H)).toBe(u);
+			});
+
+			test("matches k", () => {
+				expect(computeK(N, g, H)).toBe(k);
+			});
+
+			test("matches x", () => {
+				expect(computeX(s, I, P, H)).toBe(x);
+			});
+
+			test("matches v", () => {
+				expect(verifier(s, I, P, N, g, H)).toBe(v);
+			});
+
+			test("matches A", () => {
+				expect(clientPublicValue(a, N, g)).toBe(A);
+			});
+
+			test("matches B", () => {
+				expect(serverPublicValue(b, v, N, g, H)).toBe(B);
+			});
+
+			test("matches premaster secret on client", () => {
+				expect(clientPremasterSecret(a, I, P, { N, g, s, B }, H)).toBe(S);
+			});
+
+			test("matches premaster secret on server", () => {
+				expect(serverPremasterSecret(b, N, g, v, { A }, H)).toBe(S);
+			});
+		}
+	);
+
+	describe.skip("matches 1Password sample vectors", () => {
+		const I = "alice";
+		const P = "password123";
+		const H = "sha1";
+		const s = bigintFromHex(`BEB25379 D1A8581E B5A72767 3A2441EE`);
+		const N = bigintFromHex(`
+FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1 29024E08
+8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD EF9519B3 CD3A431B
+302B0A6D F25F1437 4FE1356D 6D51C245 E485B576 625E7EC6 F44C42E9
+A637ED6B 0BFF5CB6 F406B7ED EE386BFB 5A899FA5 AE9F2411 7C4B1FE6
+49286651 ECE45B3D C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8
+FD24CF5F 83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
+670C354E 4ABC9804 F1746C08 CA18217C 32905E46 2E36CE3B E39E772C
+180E8603 9B2783A2 EC07A28F B5C55DF0 6F4C52C9 DE2BCBF6 95581718
+3995497C EA956AE5 15D22618 98FA0510 15728E5A 8AAAC42D AD33170D
+04507A33 A85521AB DF1CBA64 ECFB8504 58DBEF0A 8AEA7157 5D060C7D
+B3970F85 A6E1E4C7 ABF5AE8C DB0933D7 1E8C94E0 4A25619D CEE3D226
+1AD2EE6B F12FFA06 D98A0864 D8760273 3EC86A64 521F2B18 177B200C
+BBE11757 7A615D6C 770988C0 BAD946E2 08E24FA0 74E5AB31 43DB5BFC
+E0FD108E 4B82D120 A9210801 1A723C12 A787E6D7 88719A10 BDBA5B26
+99C32718 6AF4E23C 1A946834 B6150BDA 2583E9CA 2AD44CE8 DBBBC2DB
+04DE8EF9 2E8EFC14 1FBECAA6 287C5947 4E6BC05D 99B2964F A090C3A2
+233BA186 515BE7ED 1F612970 CEE2D7AF B81BDD76 2170481C D0069127
+D5B05AA9 93B4EA98 8D8FDDC1 86FFB7DC 90A6C08F 4DF435C9 34063199
+FFFFFFFF FFFFFFFF`);
+		const g = 5n;
+		const k = bigintFromHex(`4832374a524b354d344e424a584f42434f45544356584a484641`);
+		const x = bigintFromHex(`740299d2306764ad9e87f37cd54179e388fd45c85fea3b030eb425d7adcb2773`);
+		const v = bigintFromHex(`
+7E273DE8 696FFC4F 4E337D05 B4B375BE B0DDE156 9E8FA00A 9886D812
+9BADA1F1 822223CA 1A605B53 0E379BA4 729FDC59 F105B478 7E5186F5
+C671085A 1447B52A 48CF1970 B4FB6F84 00BBF4CE BFBB1681 52E08AB5
+EA53D15C 1AFF87B2 B9DA6E04 E058AD51 CC72BFC9 033B564E 26480D78
+E955A5E2 9E7AB245 DB2BE315 E2099AFB`);
+		const a = bigintFromHex(`f1ecc95bb29e8a360e9b257d5688c83d503506a6a6eba683f1e06`);
+		const b = bigintFromHex(
+			`E487CB59 D31AC550 471E81F0 0F6928E0 1DDA08E9 74A004F4 9E61F5D1 05284D20`
+		);
+		const A = clientPublicValue(a, N, g);
+		const B = bigintFromHex(
+			`780a5495cbf731d2463fd01d28822e7d9ccf697c4239d5151f85666aa06b3767e0301b54cfad3bd2b526d4d8a1d96492e59c8d8ecddca96b7e288f186155ffa57b50df6bc2103b6004400b797334a22d9dd234b40142a5ab714ea6070d2ed55096049f50efba99862b72f7e7aee51ed71ba6663fff570cc713d456316f3535630e87a245f09b0791c6e687baa65bf2dfb5c17e50c250256cdad4c9851a2484e88326888060ae9578b5a60e0c85143b25f4fb4fca794e266a4359642da085672d6a3b881649a387875685aeb1ae3d809bf7818dcad596c6e29d566ae87c0ad645a0fcc2eb4f066c097670adf48cf0954918fda4dc30588261321d592f890eed87a950d387b48cf6b4a49f9d497323f683091ae6a4efe675d6bfc4393c0c3d54c9adad65b8dd3a7b7e85cd5d31e97bebc8f23b370348dab53903ec5085cbf65de5e5491f417e5bf9953f081e788f36c26cbe00664a1256c4befb00765ea7e432af189521442c186f14442b1957e444426f740f363ebda943da2bb3b18a13e2f41be9cc3ca0a1b111f6983f9b8d0ee0f4b573c6042fbc0ca029821ebe517ed0755a94f42d32b0abef9240af0f37b5fe0e90c4ca83acf91d28a7f3acff5657bf69fdb7747e380b23fd437f637da2f7ebcf8733a69a75715fe3894e1799906b48e3ae818332cf5f9533e7af5a1f065f907c8f31fe778fa2da853e69926fc551d6b3ae`
+		);
+		const u = bigintFromHex(`dad353365f78590c1857b29f16e3a947df4707868e2dd2d2b4eafd35c8c854a1`);
+		const premasterSecret = bigintFromHex(
+			`f6bef3d6fa5a08a849bf61041cd5b3185c16aede851c819a3644fa7e918c4da6`
+		);
+
+		test("matches u", () => {
+			expect(computeU(A, B, N, H)).toBe(u);
+		});
+
+		test("matches premaster secret", () => {
+			// expect(clientPremasterSecret(a, I, P, { N, g, s, B }, H)).toBe(premasterSecret);
+			expect(serverPremasterSecret(b, N, g, v, { A }, H)).toBe(premasterSecret);
+		});
+	});
+
+	describe("verifies", () => {
+		test("good password", () => {
+			// Client
+			const a = randomBits(256);
+			const I = "alice";
+			const P = "password1234";
+
+			// Server
+			const H = "sha1";
+			const N = bigintFromHex(`
+EEAF0AB9 ADB38DD6 9C33F80A FA8FC5E8 60726187 75FF3C0B 9EA2314C
+9C256576 D674DF74 96EA81D3 383B4813 D692C6E0 E0D5D8E2 50B98BE4
+8E495C1D 6089DAD1 5DC7D7B4 6154D6B6 CE8EF4AD 69B15D49 82559B29
+7BCF1885 C529F566 660E57EC 68EDBC3C 05726CC0 2FD4CBF4 976EAA9A
+FD5138FE 8376435B 9FC61D2F C0EB06E3`);
+			const g = 2n;
+			const b = randomBits(256);
+			const s = randomBits(128);
+
+			// Client
+			const v = verifier(s, I, P, N, g, H);
+			const A = clientPublicValue(a, N, g);
+
+			// Server
+			const B = serverPublicValue(b, v, N, g, H);
+			const S_server = serverPremasterSecret(b, N, g, v, { A }, H);
+
+			// Client
+			const S_client = clientPremasterSecret(a, I, P, { N, g, s, B }, H);
+
+			expect(S_client).toBe(S_server);
+
+			// TODO: Compute K, M1, M2
+		});
+	});
+
+	describe("Modular exponentiation", () => {
+		test.each([
+			[0n, 0n, 1n, 1n],
+			[1n, 0n, 1n, 1n],
+			[2n, 0n, 1n, 1n],
+			[2n, 1n, 2n, 0n],
+			[4n, 1n, 2n, 0n],
+			[2n, 2n, 2n, 0n],
+		])("simple values: %i^%i mod %i = %i", (x, y, N, result) => {
+			expect(modExp(x, y, N)).toBe(result);
+		});
+	});
+
+	describe("Hashing", () => {
+		test.each([
+			// these values from DuckDuckGo:
+			["", bigintFromHex("da39a3ee5e6b4b0d3255bfef95601890afd80709")],
+			["test", bigintFromHex("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3")],
+			["calculator", bigintFromHex("46237b3d702aad617eae653792a6269e836aa44d")],
+		])("SHA1('%s') = %i", (data, result) => {
+			expect(HASH("sha1", data, "utf8")).toBe(result);
+		});
+
+		test.each([
+			// these values from DuckDuckGo:
+			["test", bigintFromHex("928b20366943e2afd11ebc0eae2e53a93bf177a4fcf35bcc64d503704e65e202")],
+			[
+				"calculator",
+				bigintFromHex("25777f7298db3b9b6200b0284d71b9755a5956839b7bc45aa27e1076a1f12012"),
+			],
+		])("BLAKE2b-256('%s') = %i", (data, result) => {
+			expect(HASH("blake2b-256", data, "utf8")).toBe(result);
+		});
+	});
+
+	describe("Left-pad", () => {
+		test.each([
+			[0n, 0n, "0"],
+			[5n, 0n, "5"],
+			[5n, 16n, "05"],
+			[5n, 162098n, "00005"],
+			[bigintFromHex("f"), 162098n, "0000f"],
+			[bigintFromHex("96"), 162098n, "00096"],
+			[bigintFromHex("16f166"), 162098n, "16f166"],
+		])("PAD(%i, %i) = '%s'", (x, N, result) => {
+			expect(PAD(x, N)).toBe(result);
+		});
+	});
+});
---
+++ server/auth/srp.ts
@@ -0,0 +1,433 @@
+/*
+ * Implementation based on https://www.rfc-editor.org/rfc/rfc5054
+ */
+
+import type { Encoding } from "node:crypto";
+import { createHash, createHmac, randomBytes } from "node:crypto";
+import { requireEnv } from "../environment";
+import _safeCompare from "safe-compare";
+import BLAKE2s from "blake2s-js";
+import blake2b from "blake2b";
+
+/*
+ * Notation (§2.1)
+ */
+
+// let N: bigint; // a safe prime "of the form N=2q+1, where q is also prime", MUST be "large enough to make calculating discrete logarithms computationally infeasible". See Appendix A for trusted sources from which to receive group parameters. Client may also use parameters configured locally "by a trusted administrator"
+// let g: bigint; // generator, see §2.5.3 to create this and Appendix A for usable examples
+// let s: bigint; // salt
+// let B: bigint; // server's public value
+// let b: bigint; // server's private value, "a random number that SHOULD be at least 256 bits in length"
+// let A: bigint; // client's public value
+// let a: bigint; // client's private value, "a random number that SHOULD be at least 256 bits in length"
+// let I: string; // username
+// let P: string; // password
+// let v: bigint; // verifier
+// let k: bigint; // SRP-6 multiplier
+
+// string concatenation ("|" in spec, use template strings, the `+` operator, or `.concat`)
+// exponentiation ("^" in spec, use `**`)
+// "integer remainder"... modulo? ("%" in spec, use `%`)
+
+export function PAD(x: bigint, N: bigint): string {
+	const nLength = N.toString(16).length;
+	return x.toString(16).padStart(nLength, "0");
+}
+
+// username and password strings must be utf-8 unicode (§2.3)
+
+// Be sure to do this over TLS, to handle the "certificate" parts of the SRP spec
+
+// **
+// ** Handshake Protocol (§2.2) **
+// **
+
+export function onClientHello(I: string, v: bigint, N: bigint, g: bigint): void {
+	const s = undefined as unknown as bigint; // the user's salt from the username, or send a fake one if username is unknown
+	const b = randomBits(256);
+	const B = serverPublicValue(b, v, N, g, "sha1");
+	sendServerHello(N, g, s, B);
+}
+
+function sendServerHello(N: bigint, g: bigint, s: bigint, B: bigint): void {
+	// Server Hello
+	// Server Key Exchange (N, g, s, B)
+	// Server Hello Done
+}
+
+export function onClientKeyExchange(A: string): void {
+	// [Change cipher spec]
+	// Finished
+	// ... give the user their JWT i guess
+}
+
+// **
+// ** Verifier Creation (§2.4) **
+// **
+
+export function computeX(s: bigint, I: string, P: string, algorithm: HashAlgorithm): bigint {
+	return HASH(algorithm, s.toString(16) + HASH(algorithm, `${I}:${P}`, "utf8").toString(16), "hex");
+}
+
+/**
+ * @param s The user's salt
+ * @param I The user's given username
+ * @param P The user's password
+ * @param N Group parameter N
+ * @param g Group parameter g
+ * @param algorithm The hashing algorithm to use
+ */
+export function verifier(
+	s: bigint,
+	I: string,
+	P: string,
+	N: bigint,
+	g: bigint,
+	algorithm: HashAlgorithm
+): bigint {
+	const x = computeX(s, I, P, algorithm);
+	return modExp(g, x, N);
+}
+
+// **
+// ** Client Hello (§2.5.1) **
+// **
+
+// Note on session resumption (§2.5.1.1):
+// Client needs to include the "SRP extension" in the hello message; do full handshake in case server can't or won't resume the session (expiration?).
+// If server doesn't want to resume the session, ignore the SRP extension "except for its inclusion in the finished message hashes".
+
+// Note on unknown SRP info from client (§2.5.1.3):
+// If there's no known verifier for the given username, respond with a "bad_record_mac" alert (see §2.9), to indicate that the password was wrong. (Matches legacy password-auth behavior.) Be sure to send a consistent salt (s) and group (N,g) values for the same username, to simulate the existence of an entry for the given username. For B, return a random value between 1 and N-1 inclusive, while simulating computation delays.
+// - Generate a fake verifier using process.env.AUTH_SECRET for B
+
+function fakeSaltForUnknownUsername(I: string): bigint {
+	return HMAC_SHA1(requireEnv("AUTH_SECRET"), `salt${I}`);
+}
+
+// **
+// ** Server Key Exchange (§2.5.3) **
+// **
+
+type ServerKeyMessage = ServerSRPParams;
+
+// function getStoredVerifierForUsername(I: string): bigint {
+// 	// read the database, get stored v
+// }
+
+/**
+ * Do a constant time integer comparison. Always compare the complete numbers
+ * against each other to get a constant time. This method does not short-cut
+ * if the two number's length differs.
+ */
+function safeCompare(x: bigint, y: bigint): boolean {
+	const a = x.toString(16);
+	const b = y.toString(16);
+	return _safeCompare(a, b);
+}
+
+export function computeK(N: bigint, g: bigint, algorithm: HashAlgorithm): bigint {
+	return HASH(algorithm, N.toString(16) + PAD(g, N), "hex");
+}
+
+/**
+ * @param b The server's private value
+ * @param v The user's stored verifier
+ * @param N Group parameter N
+ * @param g Group parameter g
+ */
+export function serverPublicValue(
+	b: bigint,
+	v: bigint,
+	N: bigint,
+	g: bigint,
+	algorithm: HashAlgorithm
+): bigint {
+	const k = computeK(N, g, algorithm);
+	const B = modAdd(k * v, modExp(g, b, N), N);
+	if (safeCompare(B % N, 0n)) throw new IllegalParameterError(); // CRUCIAL for security
+	return B;
+}
+
+// **
+// ** Client Key Exchange (§2.5.4) **
+// **
+
+type ClientKeyMessage = ClientSRPPublic;
+
+/**
+ * @param a The client's private value
+ * @param N Group parameter N
+ * @param g Group parameter g
+ */
+export function clientPublicValue(a: bigint, N: bigint, g: bigint): bigint {
+	const A = modExp(g, a, N);
+	if (safeCompare(A % N, 0n)) throw new IllegalParameterError(); // CRUCIAL for security
+	return A;
+}
+
+// **
+// ** Premaster Secret (§2.6) **
+// **
+
+class BadRecordMacError extends Error {
+	readonly code: "bad_record_mac"; // the client should tell the user that the username/password are wrong
+
+	constructor() {
+		super("bad_record_mac");
+		this.name = "BadRecordMacError";
+		this.code = "bad_record_mac";
+	}
+}
+
+export function computeU(A: bigint, B: bigint, N: bigint, algorithm: HashAlgorithm): bigint {
+	return HASH(algorithm, PAD(A, N) + PAD(B, N), "hex");
+}
+
+/**
+ * @param a The client's private value
+ * @param I The user's given username
+ * @param P The user's given password
+ * @param skm Key data from the server
+ * @param algorithm The hashing algorithm to use
+ */
+export function clientPremasterSecret(
+	a: bigint,
+	I: string,
+	P: string,
+	skm: ServerKeyMessage,
+	algorithm: HashAlgorithm
+): bigint {
+	const { N, g, s, B } = skm;
+	const A = clientPublicValue(a, N, g);
+	const u = computeU(A, B, N, algorithm);
+	const k = computeK(N, g, algorithm);
+	const x = computeX(s, I, P, algorithm);
+	if (safeCompare(B % N, 0n)) throw new IllegalParameterError(); // CRUCIAL for security
+
+	// S = (B - (k * g^x)) ^ (a + (u * x)) % N
+	// See https://github.com/symeapp/srp-client/blob/5d3b61b19cf3a6319df05605abf89f83a411c29f/src/srp-client.js#L174
+	const Bp = B + N * k; // Why do we transform B in this way? Seems non-standard
+	const bx = modExp(g, x, N);
+	const S = modExp(Bp - k * bx, u * x + a, N);
+	return S;
+}
+
+/**
+ * @param b The server's private value
+ * @param N Group parameter N
+ * @param g Group parameter g
+ * @param v The user's password verifier
+ * @param ckm The client's key message
+ * @param algorithm The hashing algorithm to use
+ */
+export function serverPremasterSecret(
+	b: bigint,
+	N: bigint,
+	g: bigint,
+	v: bigint,
+	ckm: ClientKeyMessage,
+	algorithm: HashAlgorithm
+): bigint {
+	const { A } = ckm;
+	const B = serverPublicValue(b, v, N, g, algorithm);
+	const u = HASH(algorithm, PAD(A, N) + PAD(B, N), "hex");
+	if (safeCompare(A % N, 0n)) throw new IllegalParameterError(); // CRUCIAL for security
+
+	// S = (A * v^u) ^ b % N
+	const S = modExp(A * modExp(v, u, N), b, N);
+	return S;
+}
+
+function assertSecretsMatch(M1: bigint, M2: bigint): void {
+	if (!safeCompare(M1, M2)) throw new BadRecordMacError();
+}
+
+// **
+// ** Server Key Exchange (§2.8.2) **
+// **
+
+// Auth should accept a KeyExchangeAlgorithm value. If that is "srp", use this file's methods should be used for verifying the password. (We can use the same endpoint! Only need client to inform server there whether we're using SRP, defaulting to legacy password auth)
+
+interface ServerSRPParams {
+	readonly N: bigint;
+	readonly g: bigint;
+	readonly s: bigint;
+	readonly B: bigint;
+}
+
+// **
+// ** Client Key Exchange (§2.8.3) **
+// **
+
+interface ClientSRPPublic {
+	readonly A: bigint;
+}
+
+// **
+// ** Error Alerts (§2.9) **
+// **
+
+/**
+ * Sent by a client that receives unknown or untrusted (N, g) values.
+ */
+export class InsufficientSecurityError extends Error {
+	readonly code: "insufficient_security";
+	readonly num: 71;
+
+	constructor() {
+		super("insufficient_security");
+		this.name = "InsufficientSecurityError";
+		this.code = "insufficient_security";
+		this.num = 71;
+	}
+}
+
+/**
+ * Sent by a client or server that receives a key exchange message with A % N = 0 or B % N = 0.
+ */
+class IllegalParameterError extends Error {
+	readonly code: "illegal_parameter";
+	readonly num: 47;
+
+	constructor() {
+		super("bad_record_mac");
+		this.name = "IllegalParameterError";
+		this.code = "illegal_parameter";
+		this.num = 47;
+	}
+}
+
+// **
+// ** Important consideratons (§3.3) **
+// **
+
+// The threat model of SRP does not protect user passwords from dictionary attacks, even if the password file is stolen.
+
+// **
+// ** On the use of SHA-1 (§3.4) **
+// **
+
+// Collisions don't compromise our use of this cipher.
+// Clients should send the cipher suite in the Client Hello message, because we'll need a way to migrate in case a viable attack on SHA-1 is discovered.
+
+// **
+// ** Operations not defined in the spec **
+// **
+
+const hashAlgorithms = [
+	// From OpenSSL on macOS
+	"gost-mac",
+	"md4",
+	"md5",
+	"md_gost94",
+	"ripemd160",
+	"sha1",
+	"sha224",
+	"sha256",
+	"sha384",
+	"sha512",
+	"streebog256",
+	"streebog512",
+	"whirlpool",
+
+	// Handled specially
+	"blake2s-256",
+	"blake2b-224",
+	"blake2b-256",
+	"blake2b-384",
+	"blake2b-512",
+] as const;
+
+type HashAlgorithm = typeof hashAlgorithms[number];
+
+export function assertHashAlgorithm(tbd: string): asserts tbd is HashAlgorithm {
+	if (!hashAlgorithms.includes(tbd as HashAlgorithm))
+		throw new TypeError(`Value '${tbd}' does not match any of ${hashAlgorithms.join(", ")}`);
+}
+
+export function HASH(algorithm: HashAlgorithm, data: string, inputEncoding: Encoding): bigint {
+	if (algorithm === "blake2s-256") {
+		const hash = new BLAKE2s();
+		hash.update(Buffer.from(data, inputEncoding));
+		return BigInt(`0x${hash.hexDigest()}`);
+	}
+
+	if (algorithm === "blake2b-256") {
+		const hash = blake2b(256 / 8);
+		hash.update(Buffer.from(data, inputEncoding));
+		return BigInt(`0x${hash.digest("hex")}`);
+	}
+
+	if (algorithm === "blake2b-224" || algorithm === "blake2b-384" || algorithm === "blake2b-512") {
+		let outLength: number;
+		switch (algorithm) {
+			case "blake2b-224":
+				outLength = 224 / 8;
+				break;
+			case "blake2b-384":
+				outLength = 384 / 8;
+				break;
+			case "blake2b-512":
+				outLength = 512 / 8;
+				break;
+		}
+		const hash = blake2b(outLength);
+		hash.update(Buffer.from(data, inputEncoding));
+		return BigInt(`0x${hash.digest("hex")}`);
+	}
+
+	const hash = createHash(algorithm);
+	hash.update(data, inputEncoding);
+	return BigInt(`0x${hash.digest("hex")}`);
+}
+
+function HMAC_SHA1(key: string, data: string): bigint {
+	const hmac = createHmac("sha1", key);
+	hmac.update(data);
+	return BigInt(`0x${hmac.digest("hex")}`);
+}
+
+export function randomBits(bitCount: number): bigint {
+	if (bitCount % 8 !== 0)
+		throw new TypeError(`Given bit count '${bitCount}' must be a multiple of 8`);
+	const numBytes = bitCount / 8;
+	const bytes = randomBytes(numBytes);
+	return BigInt(`0x${bytes.toString("hex")}`);
+}
+
+function isEven(x: bigint): boolean {
+	return safeCompare(x % 2n, 0n);
+}
+
+/**
+ * Performs modular addition.
+ *
+ * @param x The integer to add
+ * @param y The integer by which to add
+ * @param N The largest value of any intermediate operation
+ * @returns The modulated result of the exponentiation: x+y mod N
+ */
+function modAdd(x: bigint, y: bigint, N: bigint): bigint {
+	return ((x % N) + (y % N)) % N;
+}
+
+/**
+ * Performs modular exponentiation.
+ *
+ * @param x The integer to multiply
+ * @param y The exponent
+ * @param N The largest value of any intermediate operation
+ * @returns The modulated result of the exponentiation: x^y mod N
+ */
+export function modExp(x: bigint, y: bigint, N: bigint): bigint {
+	if (safeCompare(y, 0n)) {
+		return 1n;
+	}
+
+	const z = modExp(x, y / 2n, N);
+	if (isEven(y)) {
+		return z ** 2n % N;
+	}
+	return (x * z ** 2n) % N;
+}
