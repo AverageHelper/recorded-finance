b65bcd1962e0fbdff1cecd386601e7953718792e
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-11-11 12:39:42 -0700

    chore: Better define the logger interfaces

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,21 @@
 /**
+ * The basic interface used by the Console API.
+ */
+interface BaseLogger {
+	debug: (message?: unknown, ...optionalParams: Array<unknown>) => void;
+	info: (message?: unknown, ...optionalParams: Array<unknown>) => void;
+	warn: (message?: unknown, ...optionalParams: Array<unknown>) => void;
+	error: (message?: unknown, ...optionalParams: Array<unknown>) => void;
+}
+
+interface Logger extends BaseLogger {}
+
+/**
  * A simple logging interface that may send its messages to
  * an aggregation utility. STDOUT and STDERR are only used in
  * debug modes.
  */
-export const logger = {
+export const logger: Logger = {
 	// TODO: Do better
 	debug: (message?: unknown, ...optionalParams: Array<unknown>): void =>
 		debugLogger.debug(message, ...optionalParams),
@@ -15,11 +27,14 @@ export const logger = {
 		debugLogger.error(message, ...optionalParams),
 };
 
+// TODO: Audit logger messages for personal data, and fix that
+// TODO: Transmit logger messages somewhere I can use them for debugging
+
 /**
  * A simple logging interface that only sends its messages to
  * STDOUT and STDERR.
  */
-export const debugLogger = {
+export const debugLogger: Logger = {
 	/* eslint-disable no-console */
 	debug: (message?: unknown, ...optionalParams: Array<unknown>): void =>
 		console.debug(message, ...optionalParams),
